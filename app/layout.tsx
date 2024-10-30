4f8068528f30db6dd531e84e9cfee07aa5f34281
hggnomm <hggnomm.dev@gmail.com>
2024-10-30 17:56:01 +0700

    layout in next

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1 className="text-3xl">HIHI</h1>
         {children}
       </body>
     </html>
